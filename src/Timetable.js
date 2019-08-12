0eb92f30e40ab70d97cc9939d8064e4a5cb9e1a9
Mace <maceplus@gmail.com>
2019-08-12 16:31:35 +0100

    timeTable functions set

--- src/Timetable.js
+++ src/Timetable.js
@@ -1,92 +1,49 @@
 import React from 'react';
 
+const getAppointment = (day, hour, appointments) => {
+  return appointments.find((appointment) => {
+    return appointment.day === day && appointment.startTime === hour;
+  });
+}
 
-const CLASSES = [
+const columnHeaders = (days) => ['', ...days].map(day => <th key={day}>{day}</th>);
 
-    {
-        Time: 7,
-        Day: 1,
-        Class: "Yoga",
-        End: 8,
-        Instructor: "Maja"
+const slot = (appointment) => (
+  <>
+    <h5>{appointment.description}</h5>
+    <p>{appointment.instructor}</p>
+  </>
+)
+const columns = (days, hour, appointments) => {
+  const getDay = (day, index) => {
+    const appointment = getAppointment(index, hour, appointments);
+    return (<td key={day}>{appointment ? slot(appointment) : null}</td>)
+  }
+  return (
+    <>
+      <td>{`${hour}:00`}</td>
+      {days.map(getDay)}
+    </>
+  );
+}
 
-    },
-    {
-        Time: 7,
-        Day: 0,
-        Class: "Yoga",
-        End: 8,
-        Instructor: "Maja"
+const rows = (days, hours, appointments) => hours.map(hour =>
+   (<tr key={hour}>{columns(days, hour, appointments)}</tr>));
 
-    },
-    {
-        Time: 9,
-        Day: 4,
-        Class: "yoga",
-        End: 11,
-        Instructor: "Maja"
-    },
-    {
-        Time: 7,
-        Day: 3,
-        Class: "Boxing",
-        End: 8,
-        Instructor: "Mace"
-    },
-    {
-        Time: 12,
-        Day: 7,
-        Class: "yogalaties",
-        End: 2,
-        Instructor: "Pat"
-    },
-
-];
-
-
-
-export const getClasses = async () => {
-  return new Promise((resolve) => {
-    setTimeout(() => resolve(CLASSES), 500)
-  })
-} 
-
-// export default getClasses;
-
-
-// export const Timetable = (props) => {
-//     const {columnHeaders, rows} = props
-//     return (
-//         <div className="App">
-//         <h1>Mace Gyms</h1>
-//         <h2>Time Table</h2>
-//         <table className="time-table">
-//           <thead>
-//             <tr>{columnHeaders}</tr>
-//           </thead>
-//           <tbody>
-//             {rows}
-//           </tbody>
-//         </table>
-//       </div>
-//     );
-// };
-
-// export const Timetable = {
-//    const [] = useState([""])
-
-//   return (
-//       <div className="App">
-//       <h1>Mace Gyms</h1>
-//       <h2>Time Table</h2>
-//       <table className="time-table">
-//         <thead>
-//           <tr>{columnHeaders}</tr>
-//         </thead>
-//         <tbody>
-//           {rows}
-//         </tbody>
-//       </table>
-//     </div>
-//   );
-// };
+export const Timetable = (props) => {
+  const { days, hours, appointments } = props
+  return (
+    <div className="App">
+      <h1>Mace Gyms</h1>
+      <h2>Time Table</h2>
+      <table className="time-table">
+        <thead>
+          <tr>{columnHeaders(days)}</tr>
+        </thead>
+        <tbody>
+          {rows(days, hours, appointments)}
+        </tbody>
+      </table>
+    </div>
+  );
+};
