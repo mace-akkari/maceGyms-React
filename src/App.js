b8cd32e9d12235bf642392489f803d0706688206
Mace <maceplus@gmail.com>
2019-08-12 16:38:30 +0100

    refactor code

--- src/App.js
+++ src/App.js
@@ -1,36 +1,23 @@
-import React , { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import { getClasses } from './Timetable';
+import { getClasses } from './ExampleAPIData';
+import { Timetable } from "./Timetable"
 
-
-const DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-const HOURS = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
-
-const columnHeaders = ['', ...DAYS].map(days => <th key={days}>{days}</th>);
-
-const headers = (heading) => <><td>{heading}</td>{DAYS.map(days => (<td key={days}></td>))}</>
-
-const rows = HOURS.map(hours => (<tr key={hours}>{headers(hours + `:00`)}</tr>));
+const DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+const HOURS = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
 
 function App() {
-  useEffect(()=> {
-    getClasses().then((days) => {console.log(days)})
+  const [appointments, setAppointments] = useState([]);
+  useEffect(() => {
+    getClasses().then((classes) => { setAppointments(classes) })
   }, []);
-  return ( 
+  return (
     <div className="App">
-        <h1>Mace Gyms</h1>
-        <h2>Time Table</h2>
-        <table className="time-table">
-          <thead>
-            <tr>{columnHeaders}</tr>
-          </thead>
-          <tbody>
-            {rows}
-          </tbody>
-        </table>
-      </div>
-   //<Timetable days={DAYS} hours={HOURS} /> 
+      <h1>Mace Gyms</h1>
+      <h2>Time Table</h2>
+      <Timetable days={DAYS} hours={HOURS} appointments={appointments} />
+    </div>
   );
- }
+}
 
 export default App;
