bba15853a96887cf46965af71360c7edd89d801f
Mace <maceplus@gmail.com>
2019-08-20 16:31:12 +0100

    add homepage

---
+++ src/Homepage.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Home extends Component {    
+  
+  render() {
+    return (
+      <div className="main-content home">
+        <h2>Welcome to Mace Gyms</h2>
+        <p>For a fantastic gym in Balham, visit Mace Gyms. Ideally located in south-west London, it will cover all of your health and fitness needs.</p>
+        <p>Located only a two-minute walk from Tooting Bec Tube station and a ten-minute walk from Balham train station, 
+          Fitness First is a place to not only workout, but to relax and socialise too. </p>
+        <p>Fitness First Balham offers an extensive range of facilities. Daily group exercise classes are available 
+          and include aerobics, boxing, spin, yoga, circuit training, Zumba, pilates, core and HIIT sessions. Other 
+          facilities on offer at our Balham gym include a cardio theatre, spin studio, sauna and a freestyle floor. 
+          Personal training sessions are also available for those wanting extra help defining their workout routines and health targets. </p>
+        <p>Want to come and visit us? You can sign up for a free day pass at Fitness First Balham and experience our fantastic facilities for yourself. 
+          All of our staff are helpful and are highly-trained instructors, so come and talk to us about your fitness goals today.</p>
+      </div>
+
+    );
+  }
+}
+
+export default Home;
