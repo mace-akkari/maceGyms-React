b9a45e0c536cf271b2193f7e603c2e0fcc659181
Mace <maceplus@gmail.com>
2019-08-20 16:30:22 +0100

    add import links & routs

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import { getClasses } from './ExampleAPIData';
+
+
+import Nav from './Nav';
+import Home from './Homepage';
+// import Teachers from './Teachers';
 import { Timetable } from "./Timetable"
+// import Courses from './courses';
 
 const DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 const HOURS = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
-
-// import Nav from './Nav';
-// import Home from './Home';
-// import Teachers from './Teachers';
-// import Courses from './courses';
-
-
 
 function App() {
   const [appointments, setAppointments] = useState([]);
@@ -22,9 +21,8 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-
-
-
+        <Nav />
+        <Route exact path='/' component={Home} />
         <Route path="/timetable" render={() =>
           <Timetable days={DAYS} hours={HOURS} appointments={appointments} />} />
       </div>
