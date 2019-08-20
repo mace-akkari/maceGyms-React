c56c437d1f5b70c81639274786b386a05ca86919
Mace <maceplus@gmail.com>
2019-08-20 16:13:20 +0100

    add timetable link

--- src/Nav.js
+++ src/Nav.js
@@ -6,6 +6,7 @@ const Nav = () => (
     <ul className="main-nav">
       <li><NavLink exact to="/">Home</NavLink></li>
       <li><NavLink to="/about">About</NavLink></li>
+      <li><NavLink to="/timetable">Time Table</NavLink></li>
       <li><NavLink to="/teachers">Teachers</NavLink></li>
       <li><NavLink to="/courses">Courses</NavLink></li>
     </ul>    
