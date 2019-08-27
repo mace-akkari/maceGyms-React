11aa285be6adb56860cc8c97dfec28a816a4cbf7
Mace <maceplus@gmail.com>
2019-08-27 13:18:41 +0100

    refactor code & new page imports

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
-import { getClasses } from './ExampleAPIData';
-
 
 import Nav from './Nav';
 import Home from './Homepage';
-// import Teachers from './Teachers';
+import Teachers from './Teachers';
 import { Timetable } from "./Timetable"
+import TimeTablePage from './TimeTablePage';
 // import Courses from './courses';
 
-const DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-const HOURS = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
+
 
 function App() {
-  const [appointments, setAppointments] = useState([]);
-  useEffect(() => {
-    getClasses().then((classes) => { setAppointments(classes) })
-  }, []);
   return (
     <BrowserRouter>
       <div className="App">
         <Nav />
         <Route exact path='/' component={Home} />
-        <Route path="/timetable" render={() =>
-          <Timetable days={DAYS} hours={HOURS} appointments={appointments} />} />
+        <Route path="/Teachers" component={Teachers}/>
+        <Route path="/timetable" component={TimeTablePage} />
       </div>
     </BrowserRouter>
   );
