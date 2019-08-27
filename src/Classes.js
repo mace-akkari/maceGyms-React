4b7c2ab36122d45bc9d1957c87a1fc8748b5ab4c
Mace <maceplus@gmail.com>
2019-08-27 17:55:30 +0100

    add classes page

---
+++ src/Classes.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Classes = () => (
+    <div className="Classes-section">
+    <h2>What do we offer</h2>
+    
+    <h4>Yoga</h4>
+    <p>Real happiness comes when a person achieves the actual goal of yoga, 
+      which is optimum physical, mental and spiritual well-being. By practicing yoga as a whole, 
+      including living a yoga lifestyle and especially by practicing Yoga Sound Meditation, 
+      you can become truly happy and fulfilled in the core of your being.</p>
+
+    <h4>Boxing</h4>
+    <p>Boxing is one of the oldest and most efficient combat sports. 
+    You use footwork, balance, strategy, head movement, defensive skills and an array of punches 
+    to hit whilst hopefully minimising the chances of getting hit yourself.</p>
+
+    <h4>YogaLaties</h4>
+    <p>Yogalates is an ever-increasing popular exercise due to its positive effects 
+    on the body and the mind alike. Yogalates allows you to practice and improve your very own limits; 
+    you can push and stretch your body while opening up your mind.</p> <p> Working with your own limits is at the 
+    heart of Yogalates, everyone has different strengths and needs and Yogalates can always be adapted to 
+    the individual. Yogalates is superb for flexibility, muscle toning and endurance. 
+    We have complied a list of what we consider some of the top body benefits of Yogalates.</p>
+
+    <h4>Spin</h4>
+    <p>This spin bike workout combines heart-rate-revving sprints and muscle-building climbs to deliver 
+      the punch of a studio session anytime. Pair the spin class at home with this high-energy playlist, \
+      performing intervals to the chorus of your favorite songs, and we guarantee you'll forget you're riding solo.</p>
+  </div>
+
+      );
+
+export default Classes;
