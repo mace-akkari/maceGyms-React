27dd9834f5d1454aca6e262a32f0d8aeae9ceb56
Mace <maceplus@gmail.com>
2019-08-27 18:09:08 +0100

    remove about section

--- src/Nav.js
+++ src/Nav.js
@@ -6,10 +6,9 @@ const Nav = () => (
   <nav>
     <ul className="main-nav">
       <li><NavLink exact to="/">Home</NavLink></li>
-      <li><NavLink to="/about">About</NavLink></li>
       <li><NavLink to="/timetable">Time Table</NavLink></li>
       <li><NavLink to="/teachers">Teachers</NavLink></li>
-      <li><NavLink to="/courses">Courses</NavLink></li>
+      <li><NavLink to="/Classes">Classes</NavLink></li>
     </ul>    
   </nav>
 );
